5f64f2230fb3d14cb35da98b9a729ceb3373776f
Immortal-Dream <tonyliu02@outlook.com>
2025-04-09 11:18:53 +0800

    "bug: add test file of embedding function"

--- vector_service/app.js
+++ vector_service/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import http from 'http';
 import paperRouter from './routes/paper.js';
+//
 
 const app = express();
 app.use(express.json());
@@ -9,8 +10,24 @@ app.use('/', paperRouter);
 // Port number Setting
 const PORT = process.env.PORT || 10086;
 
-// Start the HTTP server
-const server = http.createServer(app);
-server.listen(PORT, () => {
-    console.log(`✅ Server running on port ${PORT}`);
-});
+// Start the server
+async function startServer() {
+    try {
+        // Initialize the embedding model first
+        // await initEmbedder();
+
+        // Then, start the HTTP server
+        const server = http.createServer(app);
+        server.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+
+
+    } catch (error) {
+        console.error('Server startup failed:', error);
+        process.exit(1);
+    }
+}
+
+// Start the server
+startServer();
