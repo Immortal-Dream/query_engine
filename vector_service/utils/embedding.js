df45a1c65e249cd3e2da7fd02f88f972f0a57068
Immortal-Dream <tonyliu02@outlook.com>
2025-04-17 08:10:44 +0800

    delete odd embedding file

--- vector_service/utils/embedding.js
+++
@@ -1,9 +0,0 @@
-const VECTOR_DIM = 768;
-// dummy embedding
-export function computeEmbedding(text) {
-    const vector = [];
-    for (let i = 0; i < VECTOR_DIM; i++) {
-        vector.push(Math.random());
-    }
-    return vector;
-}
